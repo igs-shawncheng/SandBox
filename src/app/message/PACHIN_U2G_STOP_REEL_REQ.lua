6fa0a087093c543560665a3d3d755183f49888b5
Cannonufo <cannonufo@gmail.com>
2023-10-31 18:05:05 +0800

    fixed STOP REEL PROTOCOL and add comment

--- src/app/message/PACHIN_U2G_STOP_REEL_REQ.lua
+++ src/app/message/PACHIN_U2G_STOP_REEL_REQ.lua
@@ -3,8 +3,9 @@ require("app.serialization.JsonSerialize")
 local PACHIN_U2G_STOP_REEL_REQ = class("PACHIN_U2G_STOP_REEL_REQ", cc.JsonSerialize:create())
 
 function PACHIN_U2G_STOP_REEL_REQ:ctor(reelIndex)
+    --reelIndex range 1-3
     self.StopReelReq = {}
-    self.StopReelReq.bet = reelIndex
+    self.StopReelReq.reelIndex = reelIndex
 end
 
 cc.PACHIN_U2G_STOP_REEL_REQ = PACHIN_U2G_STOP_REEL_REQ
